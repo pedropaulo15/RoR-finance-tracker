785af395a4da654d9046e2b0cad75c60e575cc65
Pedro <pedropaulo15@gmail.com>
2018-06-10 13:51:10 +0100

    Refactoring StocksController.

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,18 +1,13 @@
 # 
 class StocksController < ApplicationController
   def search
-    if params[:stock].present?
-    #Calling the self.method from model stock.rb file.
-      @stock = Stock.new_from_lookup(params[:stock])
-      if @stock
-        render partial: 'users/result'
-      else
-        flash[:danger] = 'You have entered an invalid stock ticker'
-        redirect_to my_portfolio_path
-      end
+    if params[:stock].blank?
+      flash.now[:danger] = 'You have entered an empty search string'
     else
-      flash[:danger] = 'You have entered an empty search string'
-      redirect_to my_portfolio_path
+      #Calling the self.method from model stock.rb file.
+      @stock = Stock.new_from_lookup(params[:stock])
+      flash.now[:danger] = 'You have entered an invalid stock ticker' unless @stock
     end
+    render partial: 'users/result'
   end
 end
