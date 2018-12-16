09cfa34b39fe97a957bb05341d5b28a53134f42e
Pedro <pedropaulo15@gmail.com>
2018-12-16 19:42:21 +0000

    Adding the add_friend method to build the friendship.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,4 +21,16 @@ class UsersController < ApplicationController
     end
     render partial: 'friends/result'
   end
+
+  def add_friend
+    @friend = User.find(params[:friend])
+    current_user.friendships.build(friend_id: @friend.id)
+
+    if current_user.save
+      flash[:success] = 'Friend was successfuly added'
+    else
+      flash[:danger] = 'There was something wrong with the friendship request'
+    end
+    redirect_to my_friends_path
+  end
 end
